e372aa0ca53237f6beccdc5523b641388586748b
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-08-27 14:09:51 +0530

    first

---
+++ Anguler_Spring_App/src/main/java/com/db/AngulerSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngulerSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngulerSpringAppApplication.class, args);
+	}
+
+}
