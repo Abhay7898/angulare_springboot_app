6325537cadabdf5766a07974ba017b87caf1df09
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-09-21 18:09:33 +0530

    new Abhay

---
+++ Anguler_Spring_App/src/main/java/com/db/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.db.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.db.model.Login;
+import com.db.servicesimpl.LoginServicesImpl;
+
+@RestController
+public class LoginController {
+	@Autowired
+	public LoginServicesImpl loginServicesImpl;
+	
+	@PutMapping
+	public ResponseEntity<?> createUser(Login login) {
+		return  null;
+	}
+	
+	public ResponseEntity<?> login(String usereId,String password) {
+		return null;
+	}
+}
